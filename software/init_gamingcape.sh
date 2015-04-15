9cb4fd4476eae0b83fcab46d286b0812ffc1d49a
Jason Kridner <jdk@ti.com>
2015-04-15 17:02:30 -0400

    service: updated for Debian

--- software/init_gamingcape.sh
+++ software/init_gamingcape.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-echo BEAGLEBOY > /sys/devices/bone_capemgr.8/slots
+xset s off
+xset s noblank
+#config-pin overlay BEAGLEBOY
+sleep 2
 echo 61 > /sys/class/gpio/export
 echo 49 > /sys/class/gpio/export
 echo in > /sys/class/gpio/gpio49/direction
 echo in > /sys/class/gpio/gpio61/direction
+#fceux --sound 1  --fullscreen 1 --xres 320 --yres 240 /root/ROMS/ROM.nes |tee /root/log.file
