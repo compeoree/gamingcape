6f8787f638304e689a1b8371087935fd5ccd75af
Louis McCarthy <compeoree@hotmail.com>
2017-05-11 03:28:04 +0000

    Updated I2C bus for Kernel 4.4.54-ti-r93

--- software/write_eeprom.sh
+++ software/write_eeprom.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-dd if=BEAGLEBOY-EEPROM.dump of=/sys/class/i2c-dev/i2c-1/device/1-0054/eeprom bs=1 count=96
+dd if=BEAGLEBOY-EEPROM.dump of=/sys/class/i2c-dev/i2c-2/device/2-0054/eeprom bs=1 count=96
